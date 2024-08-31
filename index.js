a4824e7b10cd53cf258ed94aff3c90836e231823
Fahim Adnan <muhammadfahim010@gmail.com>
2024-08-31 14:54:30 +0600

    added snowfall

---
+++ index.js
@@ -0,0 +1,20 @@
+
+for(let i = 0;i <100;i++){
+    const parent = document.querySelector('body')
+    const body_end = document.querySelector('#body-end')
+    const body_height = parent.clientHeight
+    const body_width = parent.clientWidth
+
+    const random_height =  Math.random() * body_height - 50
+    const random_width = (Math.random() * body_width) * 3
+
+    const custom_element = document.createElement('div')
+    custom_element.classList.add('snowflake')
+    custom_element.classList.add('snowflake-animate')
+    custom_element.style.top = `${random_height}px`
+    custom_element.style.left = `${Math.floor(random_width)}px`
+    custom_element.style.animationDelay = `${Math.random()*100}s`
+
+    document.querySelector('.snowflake-container').insertBefore(custom_element,body_end)
+    document.querySelector('.snowflake-container').style.height = `${document.body.scrollHeight-100}px`
+}
