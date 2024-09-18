4e40b9447c09dffdee1de0ac4f8049aabdf3713d
Fahim Adnan <muhammadfahim010@gmail.com>
2024-09-18 23:17:40 +0600

    design changes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
-for(let i = 0;i <100;i++){
+for (let i = 0; i < 100; i++) {
     const parent = document.querySelector('body')
     const body_end = document.querySelector('#body-end')
     const body_height = parent.clientHeight
     const body_width = parent.clientWidth
 
-    const random_height =  Math.random() * body_height - 50
+    const random_height = Math.random() * body_height - 50
     const random_width = (Math.random() * body_width) * 5
 
     const custom_element = document.createElement('div')
@@ -13,8 +13,20 @@ for(let i = 0;i <100;i++){
     custom_element.classList.add('snowflake-animate')
     custom_element.style.top = `${random_height}px`
     custom_element.style.left = `${Math.floor(random_width)}px`
-    custom_element.style.animationDelay = `${Math.random()*100}s`
+    custom_element.style.animationDelay = `${Math.random() * 100}s`
 
-    document.querySelector('.snowflake-container').insertBefore(custom_element,body_end)
-    document.querySelector('.snowflake-container').style.height = `${document.body.scrollHeight-100}px`
+    document.querySelector('.snowflake-container').insertBefore(custom_element, body_end)
+    document.querySelector('.snowflake-container').style.height = `${document.body.scrollHeight - 100}px`
 }
+
+
+document.querySelector('.hamburger-menu').addEventListener('click', () => {
+    document.querySelector('.sidenav').classList.toggle("sidenav-animate")
+})
+
+document.querySelectorAll('.sidenav>div').forEach(item => {
+
+    item.addEventListener('click', () => {
+        document.querySelector('.sidenav').classList.toggle("sidenav-animate")
+    })
+})
